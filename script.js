d46def82c73005a6112c56241a9570f3a5cc6129
Mari2909 <zmariy29@gmail.com>
2022-08-10 13:44:33 +0300

    added notes

--- script.js
+++ script.js
@@ -41,32 +41,6 @@ function setMonthCalendar(year, month) {
 
 setMonthCalendar(nowYear, nowMonth);
 
-
-function removeClass(id) {
-    let element = document.getElementById(id);
-    element.classList.remove('d-none');
-}
-
-function addClass(id) {
-    let element = document.getElementById(id);
-    element.classList.add('d-none');
-}
-
-function selectDay() {
-    document.addEventListener("click", function (e) {
-        if (e.target.className == "day" || e.target.className == "day date-now") {
-            e.target.classList.toggle("selected");
-            removeClass('container');
-        }
-        else if (e.target.className == "day selected" || e.target.className == "day date-now selected") {
-            e.target.classList.toggle("selected");
-            addClass('container');
-        }
-    });
-}
-
-selectDay()
-
 prev.onclick = function () {
     let curDate = new Date(yearContainer.textContent, monthName.indexOf(monthContainer.textContent));
 
@@ -76,7 +50,7 @@ prev.onclick = function () {
         curMonth = curDate.getMonth();
 
     setMonthCalendar(curYear, curMonth);
-    selectDay();
+    daysContainer.addEventListener("click", selectDay);
 }
 
 next.onclick = function () {
@@ -88,51 +62,125 @@ next.onclick = function () {
         curMonth = curDate.getMonth();
 
     setMonthCalendar(curYear, curMonth);
-    selectDay();
+    daysContainer.addEventListener("click", selectDay);
 }
+// Выбор дня
+
+function removeClass(id) {
+    let element = document.getElementById(id);
+    element.classList.remove('d-none');
+}
+
+function addClass(id) {
+    let element = document.getElementById(id);
+    element.classList.add('d-none');
+}
+
+
+/*function selectDay() {
+    let selectedDay = document.querySelectorAll('day');
+    console.log(selectedDay);
+    document.addEventListener("click", function (e) {
+
+
+        if (e.target.className == "day" || e.target.className == "day date-now") {
+            e.target.classList.toggle("selected");
+            removeClass('container');
+        }
+        else if (e.target.className == "day selected" || e.target.className == "day date-now selected") {
+            e.target.classList.toggle("selected");
+            addClass('container');
+        }
+    });
+}*/
+let date;
+function selectDay(event) {
+
+    if (event.target.className == "day" || event.target.className == "day date-now") {
+        event.target.classList.toggle("selected");
+        removeClass('container');
+    }
+    else if (event.target.className == "day selected" || event.target.className == "day date-now selected") {
+        event.target.classList.toggle("selected");
+        addClass('container');
+    }
+    if (document.querySelectorAll('selected')) {
+        daysContainer.removeEventListener("click", selectDay);
+    }
+    date = event.target.innerHTML + monthContainer.innerHTML + yearContainer.innerHTML;
+    return date;
+}
+
+daysContainer.addEventListener("click", selectDay);
 
 
 // Добавление заметок
 
 const button = document.querySelector('button');
 let notes = [];
+let textarea = document.querySelector('textarea');
+let timeInput = document.querySelector('input[type="time"]');
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    let lastNotes = localStorage.getItem('notes');
+document.addEventListener("click", function (event) {
+    let lastNotes = localStorage.getItem(`${date}`);
     if (lastNotes != null) {
         const arr = JSON.parse(lastNotes);
         const filterArr = arr.filter(el => el !== null);
         let lastNotesString = "";
         for (let elem of filterArr) {
-            lastNotesString += elem;
+            lastNotesString += elem.note;
+            notes.push(elem);
         }
         document.getElementById('note').innerHTML = lastNotesString;
-        notes.push(lastNotesString);
     }
 });
 
 function getNote() {
+
     let time = document.querySelector('input').value;
-    console.log(time);
+
     let text = document.querySelector('textarea').value;
-    if (text != '') {
-        const note = `<div class="timenote">${time}</div>` + text;
-        const result = `<div class="col">${note}</div>`;
-        return result;
+
+    if (text != '' && time != '') {
+        const note = `<div class="timenote" id="${time}"> ${time}</div>` + text;
+        const result = `<div class="col"> ${note}</div> `;
+        let noteObject = {
+            time: time,
+            note: result,
+            completed: '',
+        }
+        return noteObject;
     } else {
-        return alert("Пустая заметка!");
+        if (time == '') return alert("Не выбрано время!");
+        if (text == '') return alert("Пустая заметка!");
     }
+
 }
 
-function commentOutput() {
+function addNote() {
     notes.push(getNote());
-    localStorage.setItem('notes', JSON.stringify(notes));
-    notes = JSON.parse(localStorage.getItem('notes'));
+    localStorage.setItem(`${date}`, JSON.stringify(notes));
+    notes = JSON.parse(localStorage.getItem(`${date}`));
     const filterNotes = notes.filter(el => el !== null);
     let notesString = "";
     for (let note of filterNotes) {
-        notesString += note;
+        notesString += note.note;
     }
     document.getElementById('note').innerHTML = notesString;
 }
-button.addEventListener("click", commentOutput);
+
+
+//проверить
+/*
+function hideConteiner(elem, class1, class2) {
+    let elem = document.querySelector('selected')
+    if (elem.className == class1 || elem.className == class2) {
+        elem.classList.toggle("selected");
+        addClass('container');
+    }
+}
+
+hideConteiner(elem, "day selected", "day date-now selected");
+*/
+
+button.addEventListener("click", addNote);
